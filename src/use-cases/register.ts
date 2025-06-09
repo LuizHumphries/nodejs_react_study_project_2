9afe85b48e34c996f84d636184ecde471f71bfe3
Luiz Gustavo <luizgustavo.frota@outlook.com>
2025-06-08 23:54:28 -0300

    Starting Tests creating

---
+++ src/use-cases/register.ts
@@ -0,0 +1,25 @@
+import { UsersRepository } from '@/repositories/users-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUseCase {
+  constructor(private useRepository: UsersRepository) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.useRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    await this.useRepository.create({ name, email, password_hash })
+  }
+}
